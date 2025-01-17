aafb16533715aef18a0848bde12dbe0705284231
Joao Pedro Ramalho <jpv.ramalho2013@gmail.com>
2025-01-16 23:45:31 -0300

    [MOD] funcionalidade criar task

--- front-end/src/components/SearchForm.vue
+++ front-end/src/components/SearchForm.vue
@@ -1,14 +1,25 @@
 <template>
-  <form>
-    <label class="sr-only">Pesquisar</label>
-    <div>
-      <input
-        type="text"
-        @input="onInput"
-        placeholder="Pesquisar por Título ou id"
-        class="bg-gray-50 border border-gray-300 px-2"
+  <form class="flex items-center space-x-3 bg-white shadow-sm p-2 rounded-lg">
+    <!-- Ícone de busca -->
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5 text-gray-400"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1111.25 3.01l4.42 4.42a1 1 0 01-1.42 1.42l-4.42-4.42A6 6 0 012 8z"
+        clip-rule="evenodd"
       />
-    </div>
+    </svg>
+    <!-- Campo de entrada -->
+    <input
+      type="text"
+      @input="onInput"
+      placeholder="Pesquisar por ID"
+      class="w-full bg-gray-50 border border-gray-300 rounded-lg px-3 py-2 text-sm text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+    />
   </form>
 </template>
 
